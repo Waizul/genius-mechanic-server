76dbc39304e54d4a295e8354ebc0faa36c5042d0
Waizul Haque <waiul@gmail.com>
2021-10-27 19:30:25 +0600

    port

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const ObjectId = require('mongodb').ObjectId;
 console.log(ObjectId);
 app.use(cors());
 app.use(express.json());
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.q5fov.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 console.log(uri);
@@ -56,7 +56,7 @@ async function run() {
 run().catch(console.dir);
 
 app.get('/', (req, res) => {
-	res.send('eruweouriuower');
+	res.send('server');
 });
 
 app.listen(port, () => {
